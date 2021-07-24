4746cfe1f3d9d4072d0e8252b593127a26db2915
sigma13-K <sigma13@kaist.ac.kr>
2021-07-25 08:42:07 +0900

    edit codes as Counter app

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+ import React from 'react';
+ import Styled from 'styled-components/native';
+ import Counter from './Screens/Counter';
+
+ const Container = Styled.View`
+  flex: 1;
+  background-color: #EEE;
+ `;
+
+ const App = () => {
+   return (
+     <Container>
+       <Counter title="This is a Counter App" initValue={91} />
+     </Container>
+   );
+ };
+ export default App;
+ 
